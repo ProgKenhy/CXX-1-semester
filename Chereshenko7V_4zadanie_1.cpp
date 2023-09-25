8560ffab2f4f6bfc9c68fd1bbb23fff45913a77d
ProgKenhy <144048872+ProgKenhy@users.noreply.github.com>
2023-09-25 17:46:09 +0200

    Laboratory

--- Chereshenko7V_4zadanie_1.cpp
+++ Chereshenko7V_4zadanie_1.cpp
@@ -4,16 +4,14 @@ int main()
 {
     int A,B,C;
     std::cin >> A >> B >> C;
-    if (A%B == 0){
-            if (B>C){
-                std::cout << A/B+C;
-            }
-            else if (B<C){
-                std::cout << A/B-C;
-            }
-        }
-    else{
-                std::cout << (A+B)*C;
-            }
+    if ((A%B == 0) && (B>C)){
+        std::cout << A / B + C;             
+    }
+    else if ((A % B == 0) && (B<C)){
+        std::cout << A/B-C;
+    }
+    else {
+        std::cout << (A + B) * C;
+    }  
     return 0;
 }
