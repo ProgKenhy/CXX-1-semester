96d5e1983cd92eb18bcc27cf81347580d6c7ee74
ProgKenhy <144048872+ProgKenhy@users.noreply.github.com>
2023-10-26 00:34:56 +0200

    Laboratory

---
+++ Chereshenko7V_8zadanie_1.cpp
@@ -0,0 +1,122 @@
+// Дан файл, содержащий русский текст.
+//  Найти в тексте N<=2000 самых длинных слов, не содержащих одинаковых согласных.
+//  Записать найденные слова в текстовый файл в порядке невозрастания длины.
+//  Все найденные слова должны быть разными!
+#include <iostream>
+#include <fstream>
+
+char tolower(char x)
+{
+	if ('А' <= x && x <= 'Я')
+		return x += 32;
+	return x;
+}
+int wordlength(char *x)
+{
+	int len = 0;
+	while (x[len] != '\0')
+	{
+		len++;
+	}
+	return len;
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int n = 2001;
+	char word[100] = {0};
+	char result[n][100] = {0};
+	std::ifstream infile;
+	infile.open("C:\\Users\\Alexander\\.vscode\\Projects\\Files\\input.txt");
+	if (!infile.is_open())
+	{
+		std::cout << "Input file dod not find!";
+		return -1;
+	}
+	int lastpos = 0;
+	std::cout << "Input value of numbers: ";
+	int k, inminlen = 0;
+	std::cin >> k;
+	while (!infile.eof())
+	{
+		infile >> word;
+		word[0] = tolower(word[0]);
+		lastpos = wordlength(word) - 1;
+		for (int i = lastpos; i >= 0; i--)
+		{
+			if (ispunct(word[i]))
+				word[i] = '\0';
+			else
+				break;
+		}
+		if ((wordlength(word)) > wordlength(result[inminlen]))
+		{
+			int maxlen = 0;
+			bool flag = false;
+			for (int q = 0; q < k; q++)
+			{
+				flag = false;
+				for (int w = 0; w < 100; w++)
+				{
+					if (wordlength(result[q]) < wordlength(word))
+						maxlen = wordlength(word);
+					else
+						maxlen = wordlength(result[q]);
+					if ((word[w] != result[q][w]) && (w < maxlen))
+					{
+						flag = true;
+					}
+				}
+				if (!flag)
+					break;
+			}
+			if (flag)
+			{
+				for (int b = 0; b < wordlength(word); b++)
+				{
+					result[inminlen][b] = word[b];
+				}
+				for (int c = 0; c < k; c++)
+					if (wordlength(result[inminlen]) > wordlength(result[c]))
+					{
+						inminlen = c;
+					}
+			}
+		}
+	}
+	for (int i = 0; i < k - 1; i++)
+		for (int j = i + 1; j < k; j++)
+		{
+			if (wordlength(result[i]) < wordlength(result[j]))
+			{
+				for (int b = 0; b < 100; b++)
+				{
+					char tmp;
+					tmp = result[i][b];
+					result[i][b] = result[j][b];
+					result[j][b] = tmp;
+				}
+			}
+		}
+	std::ofstream outfile;
+	outfile.open("C:\\Users\\Alexander\\.vscode\\Projects\\Files\\output.txt");
+	for (int i = 0; i < k; i++)
+	{
+		std::cout << "TOP " << i + 1 << ": " << result[i] << std::endl;
+	}
+	for (int i = 0; i < k; i++)
+	{
+		if (infile.is_open())
+		{
+			outfile << "TOP " << i + 1 << ": " << result[i] << std::endl;
+		}
+		else
+		{
+			std::cout << "Output file did not find!";
+			return -2;
+		}
+	}
+	infile.close();
+	outfile.close();
+	return 0;
+}
